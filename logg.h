f96ace8730544159b3d844dcf5782d8c0b7add21
Manisha Goel <manisgoe@cisco.com>
2015-09-21 09:31:48 +0000

    Few more logging traces added

--- logg.h
+++ logg.h
@@ -5,6 +5,15 @@ void initialize_logging(int argc, char * argv[]);
 #define TRACE(severity,string) \
 	LOG(severity) << string
 
+#define TRACE_IF(severity,cond,string) \
+	LOG_IF(severity, cond) << string
+
+#define DTRACE(severity,string) \
+	DLOG(severity) << string
+
+#define VTRACE(severity,string) \
+	VLOG(severity) << string
+
 void initialize_logging(int argc, char * argv[])
 {
 	google::SetLogDestination(google::GLOG_INFO,"/root/server" );
